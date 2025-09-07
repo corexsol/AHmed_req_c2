a0343399d1d40ae5a0f90ed08b99499d54160266
corexsol <74492852+corexsol@users.noreply.github.com>
2025-09-07 10:49:30 +0300

    Delay screen transition and cache offline assets

--- service-worker.js
+++ service-worker.js
@@ -13,10 +13,30 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).catch(() => {
+        if (event.request.mode === 'navigate') {
+          return caches.match('./AhmedReqv2.html');
+        }
+      });
+    })
   );
 });
